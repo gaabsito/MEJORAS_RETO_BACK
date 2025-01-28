dbe539d8697cc28598108d7e7cd07e01b8fd8b6c
jescartinn <a27421@svalero.com>
2025-01-28 19:08:52 +0100

    adaptado clase pelicula

--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -2,12 +2,26 @@ public class Pelicula
 {
     public int PeliculaId { get; set; }
     public string Title { get; set; }
-    public string Description { get; set; }
+    public string Descripcion { get; set; }
     public string Director { get; set; }
     public string Actores { get; set; }
     public string Genero { get; set; }
     public string Clasificacion { get; set; }
-    public int Duration { get; set; }
+    public int Duracion { get; set; }
     public string ImageUrl { get; set; }
     public string CartelUrl { get; set; }
+
+    public Pelicula() {} //parameterless constructor
+
+    public Pelicula(string title, string descripcion, string director, string actores, 
+                   string genero, string clasificacion, int duracion)
+    {
+        Title = title;
+        Descripcion = descripcion;
+        Director = director;
+        Actores = actores;
+        Genero = genero;
+        Clasificacion = clasificacion;
+        Duracion = duracion;
+    }
 }
